9b34124b8843a18b7bb4883f90f91ae54237c924
mpicat <maxime.picat@coursdiderot.com>
2022-02-23 17:24:31 +0100

    MAJ

--- main.js
+++ main.js
@@ -10,21 +10,19 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length){
-      slideIndex = 1
-    } if (n < 1) {
-        slideIndex = slides.length
-    }
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  if (window.screen.width <= 650) {
-    slides[slideIndex-1].style.display = "block";
-  } else {
-      slides[slideIndex-1].style.display = "block";
-      slides[slideIndex].style.display = "block";
-      slides[slideIndex+1].style.display = "block";
-      slides[slideIndex+2].style.display = "block";
-    }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
